d1de3e2826f5bac699f8d965f12171b87b48cfbb
HelloZZZC <405954049@qq.com>
2018-09-07 00:07:02 +0800

    rm guide update index

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,8 +4,6 @@ const app = getApp()
 
 Page({
   data: {
-    motto: 'Hello World',
-    userInfo: {},
     hasUserInfo: false,
     canIUse: wx.canIUse('button.open-type.getUserInfo')
   },
@@ -18,72 +16,51 @@ Page({
   onLoad: function () {
     if (app.globalData.userInfo) {
       this.setData({
-        userInfo: app.globalData.userInfo,
         hasUserInfo: true
       })
-    } else if (this.data.canIUse){
+    } else if (this.data.canIUse) {
       // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
       // 所以此处加入 callback 以防止这种情况
       app.userInfoReadyCallback = res => {
         this.setData({
-          userInfo: res.userInfo,
           hasUserInfo: true
         })
       }
     } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
+      //在没有open-type="getUserInfo"做兼容处理
       wx.getUserInfo({
         success: res => {
-          wx.request({
-            url: 'http://wechat-server.com/api/bind',
-            method: 'POST',
-            data: {
-              encryptedData: res.encryptedData,
-              iv: res.iv,
-              data: res.rawData,
-              thirdKey: wx.getStorageSync('thirdKey')
-            },
-            header: {
-              'content-type': 'application/x-www-form-urlencoded'
-            },
-            success: res => {
-              app.globalData.userInfo = res.data
-              this.setData({
-                userInfo: res.data,
-                hasUserInfo: true
-              })
-            },
-            fail: () => {
-              console.log('bind failed');
-            }
-          })
+          bind(res);
         }
       })
     }
   },
   getUserInfo: function(e) {
-    wx.request({
-      url: 'http://wechat-server.com/api/bind',
-      method: 'POST',
-      data: {
-        encryptedData: e.detail.encryptedData,
-        iv: e.detail.iv,
-        data: e.detail.rawData,
-        thirdKey: wx.getStorageSync('thirdKey')
-      },
-      header: {
-        'content-type': 'application/x-www-form-urlencoded'
-      },
-      success: res => {
-        app.globalData.userInfo = res.data
-        this.setData({
-          userInfo: res.data,
-          hasUserInfo: true
-        })
-      },
-      fail: () => {
-        console.log('bind failed');
-      }
-    })
+    bind(e.detail)
   },
 })
+function bind(response) {
+  wx.request({
+    url: 'http://wechat-server.com/api/bind',
+    method: 'POST',
+    data: {
+      encryptedData: response.encryptedData,
+      iv: response.iv,
+      data: response.rawData,
+      thirdKey: wx.getStorageSync('thirdKey')
+    },
+    header: {
+      'content-type': 'application/x-www-form-urlencoded'
+    },
+    success: res => {
+      app.globalData.userInfo = res.data
+      //跳转至index
+      wx.redirectTo({
+        url: '../index/index',
+      })
+    },
+    fail: () => {
+      console.log('bind failed');
+    }
+  })
+}
