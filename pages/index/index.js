0a1c776a7d2d28235e52e90d4c2bfdf979f407f8
HelloZZZC <405954049@qq.com>
2018-09-07 16:51:36 +0800

    bind code update

--- pages/index/index.js
+++ pages/index/index.js
@@ -14,6 +14,7 @@ Page({
     })
   },
   onLoad: function () {
+    console.log(this);
     if (app.globalData.userInfo) {
       this.setData({
         hasUserInfo: true
@@ -36,10 +37,15 @@ Page({
     }
   },
   getUserInfo: function(e) {
-    bind(e.detail)
+    bind(e.detail, true)
   },
+  addPlan: function(e) {
+    wx.navigateTo({
+      url: '../plan/plan?mode=create',
+    })
+  }
 })
-function bind(response) {
+function bind(response, needRedirect = false) {
   wx.request({
     url: 'http://wechat-server.com/api/bind',
     method: 'POST',
@@ -54,10 +60,12 @@ function bind(response) {
     },
     success: res => {
       app.globalData.userInfo = res.data
-      //跳转至index
-      wx.redirectTo({
-        url: '../index/index',
-      })
+      //跳转至新增plan页面
+      if (needRedirect) {
+        wx.navigateTo({
+          url: '../plan/plan?mode=create',
+        })
+      }
     },
     fail: () => {
       console.log('bind failed');
---
+++ pages/plan/plan.js
@@ -0,0 +1,13 @@
+Page({
+  data: {
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+
+  },
+})
